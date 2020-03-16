e885a647e195a6eccc593069fae1716887d93bbe
Travis Brown <travisrobertbrown@gmail.com>
2020-03-16 10:49:59 -0500

    Add new generic example types and tests

--- examples/example-1/src/test/scala/io/circe/examples/ExamplesSuite.scala
+++ examples/example-1/src/test/scala/io/circe/examples/ExamplesSuite.scala
@@ -1,5 +1,6 @@
 package io.circe.examples
 
+import cats.instances.list._
 import io.circe.testing.instances._
 import io.circe.testing.golden._
 import org.scalatest.flatspec.AnyFlatSpec
@@ -9,4 +10,14 @@ import org.typelevel.discipline.scalatest.FlatSpecDiscipline
 class ExamplesSuite extends AnyFlatSpec with FlatSpecDiscipline with Configuration {
   checkAll("GoldenCodec[Foo]", GoldenCodecTests[Foo](10).goldenCodec)
   checkAll("GoldenCodec[Wub]", GoldenCodecTests[Wub].goldenCodec)
+  // Validate that golden filenames don't collide for type constructors applied
+  // to different arguments.
+  checkAll("GoldenCodec[MyList[Foo]]", GoldenCodecTests[MyList[Foo]].goldenCodec)
+  checkAll("GoldenCodec[MyList[Wub]]", GoldenCodecTests[MyList[Wub]].goldenCodec)
+
+  // Validated that deeply nested types are successfully given golden filenames.
+  checkAll(
+    "GoldenCodec[MyTuple[MyList[Wub], MyList[MyList[Foo]]]]",
+    GoldenCodecTests[MyTuple[MyList[Wub], MyList[MyList[Foo]]]].goldenCodec
+  )
 }
